3a8c82d27291b288b8f1a917859e9b25c0f8c4e9
daviloko00-00 <davicustodiosilva36@gmail.com>
2025-06-16 16:14:31 -0300

    Finalizado, Falta teste

--- index.js
+++ index.js
@@ -5,48 +5,48 @@ let distancias = [];
 let situacoes = [];
 let valores = [];
 let clientes = [];
-
+let valorestotais = []
+i = 1
 
 const fs = require("fs");
 const path = require("path");
-do{i = 1
-    let nome = promptSync(`Digite o nome do clinte ${i}: `);
-    let endereco = promptSync(`Digite o endereço do cliente ${i}: `);
-    let distancia = promptSync(`Digite a distancia ${i}: `);
-    let situacao = promptSync(`Digite a situação do cliente. 1. normal. 2. Urgente (Escreva o número 1 ou 2): `);
-    let valor = promptSync(`Digite o valor da viagem ${i} por km em reais: `);
-    if ((!isNaN(nome))| !isNaN(endereco)| isNaN(distancia)| isNaN(situacao)|isNaN(valor)){
-        console.log("INFORMAÇÕES INCORRETAS. TENTE NOVAMENTE");
-    }
-    if (situacao == 1){
+fs.writeFileSync("listagem completa.txt", "", "utf8");
+do {
+    do {
+        let nome = promptSync(`Digite o nome do clinte ${i}: `);
+        let endereco = promptSync(`Digite o endereço do cliente ${i}: `);
+        let distancia = promptSync(`Digite a distancia ${i}: `);
+        let situacao = promptSync(`Digite a situação do cliente. 1. normal. 2. Urgente (Escreva o número 1 ou 2): `);
+        let valor = promptSync(`Digite o valor da viagem ${i} por km em reais: `);
+        if ((!isNaN(nome)) | !isNaN(endereco) | isNaN(distancia) | isNaN(situacao) | isNaN(valor)) {
+            console.log("INFORMAÇÕES INCORRETAS. TENTE NOVAMENTE");
+        }
+    } while ((!isNaN(nome)) | !isNaN(endereco) | isNaN(distancia) | isNaN(situacao) | isNaN(valor));
+    if (situacao == 1) {
         situacao = "normal";
-        valor = valor;
-    }else if (situacao == 2) {
+        valortotal = valor * distancia;
+    } else if (situacao == 2) {
         situacao = "urgente";
-        valor = valor + (0.2*valor);
+        valortotal = (valor + (0.2 * valor)) * distancia;
     }
-    
-        nomes[i] = nome;
-        enderecos[i] = endereco;
-        distancias[i] = distancia;
-        situacoes[i] = situacao;
-        valores[i] = valor;
-     cliente = console.log(`nome: ${nomes[i]}\n endereço: = ${enderecos[i]}\n distancia ${distancias[i]} \n situação : ${situacoes[i]}\n valor : ${valores[i]} `)
+
+    nomes[i] = nome;
+    enderecos[i] = endereco;
+    distancias[i] = distancia;
+    situacoes[i] = situacao;
+    valorestotais[i] = valortotal;
+    cliente = console.log(`nome: ${nomes[i]}\n endereço: = ${enderecos[i]}\n distancia ${distancias[i]} \n situação : ${situacoes[i]}\n valor total : ${valorestotais[i]} \n valor por km ${valores[i]} `)
     opcao = promptSync(`Cliente registrado, caso deseja continuar tecle enter, caso não digite "n": `)
     cliente = clientes[i];
-    if (opcao == "n"){
+    if (opcao == "n") {
         console.log("saindo...")
-  
+
     }
 
+    let textoporlinha = `nome: ${nomes[i]}\n endereço: = ${enderecos[i]}\n distancia ${distancias[i]} \n situação : ${situacoes[i]}\n valor total : ${valorestotais[i]} \n valor por km ${valores[i]}`;
+    fs.appendFileSync("listagem completa.txt", textoporlinha, "utf8");
+    fs.appendFileSync("listagem completa.txt", "\n\n", "utf8")
 
-
-
-        
-    
-    
-
-}while( opcao !== "n");
-
-console.log(clientes)
-
+    i++;
+} while (opcao !== "n");
+console.log("arquivo feito com a listagem completa")
