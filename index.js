cbda57af87daf6ae8e7bcf4b7684a43a88727b30
daviloko00-00 <davicustodiosilva36@gmail.com>
2025-06-16 15:52:28 -0300

    Finalizado, falta teste

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let enderecos = [];
 let distancias = [];
 let situacoes = [];
 let valores = [];
+let clientes = [];
 
 
 const fs = require("fs");
@@ -12,23 +13,40 @@ do{i = 1
     let nome = promptSync(`Digite o nome do clinte ${i}: `);
     let endereco = promptSync(`Digite o endereço do cliente ${i}: `);
     let distancia = promptSync(`Digite a distancia ${i}: `);
-    let situacao = promptSync(`Digite a situação do cliente. 1. nomrmal. 2. Urgente: `);
+    let situacao = promptSync(`Digite a situação do cliente. 1. normal. 2. Urgente (Escreva o número 1 ou 2): `);
     let valor = promptSync(`Digite o valor da viagem ${i} por km em reais: `);
-    let cliente = 
+    if ((!isNaN(nome))| !isNaN(endereco)| isNaN(distancia)| isNaN(situacao)|isNaN(valor)){
+        console.log("INFORMAÇÕES INCORRETAS. TENTE NOVAMENTE");
+    }
+    if (situacao == 1){
+        situacao = "normal";
+        valor = valor;
+    }else if (situacao == 2) {
+        situacao = "urgente";
+        valor = valor + (0.2*valor);
+    }
+    
         nomes[i] = nome;
         enderecos[i] = endereco;
         distancias[i] = distancia;
         situacoes[i] = situacao;
         valores[i] = valor;
-    
-    opcao = promptSync(`Cliente registrado`)
+     cliente = console.log(`nome: ${nomes[i]}\n endereço: = ${enderecos[i]}\n distancia ${distancias[i]} \n situação : ${situacoes[i]}\n valor : ${valores[i]} `)
+    opcao = promptSync(`Cliente registrado, caso deseja continuar tecle enter, caso não digite "n": `)
+    cliente = clientes[i];
+    if (opcao == "n"){
+        console.log("saindo...")
+  
+    }
 
-    console.log(nome, endereco, distancia, situacao, valor)
+
 
 
         
     
     
 
-}while( opcao != n)
+}while( opcao !== "n");
+
+console.log(clientes)
 
