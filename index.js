468c47499d873c5a3b69e6d7770e6ca283eb2ed3
daviloko00-00 <davicustodiosilva36@gmail.com>
2025-06-16 13:53:33 -0300

    Começo do código, filho chora e mãe não vê

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let nomes = [];
 let endereços = [];
 let distancias = [];
 let situações = [];
+
+
+const fs = require("fs");
+const promptSync = require("prompt-sync");
+const path = require("path");
