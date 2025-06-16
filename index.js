5bd25c0280cfb07550fe31b4969c8d41232c9be6
daviloko00-00 <davicustodiosilva36@gmail.com>
2025-06-16 15:22:29 -0300

    Parte feita, entrada de dados

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+const promptSync = require("prompt-sync")();
 let nomes = [];
 let enderecos = [];
 let distancias = [];
-let situações = [];
+let situacoes = [];
+let valores = [];
 
 
 const fs = require("fs");
-const promptSync = require("prompt-sync");
 const path = require("path");
 do{i = 1
-    let nome = promptSync(`Digite o nome do clinte ${i}: `)
-    let endereco = promptSync(`Digite o endereço do cliente ${i}: `)
-    let distancia = promptSync(`Digite a distancia ${i}: `)
-    let situacao = promptSync(`Digite a situação do cliente. 1. nomrmal. 2. Urgente: `)
-    let valor = promptSync(`Digite o valor da viagem ${i} por km em reais: `)
+    let nome = promptSync(`Digite o nome do clinte ${i}: `);
+    let endereco = promptSync(`Digite o endereço do cliente ${i}: `);
+    let distancia = promptSync(`Digite a distancia ${i}: `);
+    let situacao = promptSync(`Digite a situação do cliente. 1. nomrmal. 2. Urgente: `);
+    let valor = promptSync(`Digite o valor da viagem ${i} por km em reais: `);
+    let cliente = 
         nomes[i] = nome;
         enderecos[i] = endereco;
         distancias[i] = distancia;
         situacoes[i] = situacao;
+        valores[i] = valor;
+    
+    opcao = promptSync(`Cliente registrado`)
+
+    console.log(nome, endereco, distancia, situacao, valor)
+
 
         
     
