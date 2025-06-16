6e109c2d1977752a4bc84d6a1caad5d3486adbc9
daviloko00-00 <davicustodiosilva36@gmail.com>
2025-06-16 14:37:46 -0300

    Começo do código, filho chora nessa hora

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nomes = [];
-let endereços = [];
+let enderecos = [];
 let distancias = [];
 let situações = [];
 
@@ -7,4 +7,20 @@ let situações = [];
 const fs = require("fs");
 const promptSync = require("prompt-sync");
 const path = require("path");
-///
+do{i = 1
+    let nome = promptSync(`Digite o nome do clinte ${i}: `)
+    let endereco = promptSync(`Digite o endereço do cliente ${i}: `)
+    let distancia = promptSync(`Digite a distancia ${i}: `)
+    let situacao = promptSync(`Digite a situação do cliente. 1. nomrmal. 2. Urgente: `)
+    let valor = promptSync(`Digite o valor da viagem ${i} por km em reais: `)
+        nomes[i] = nome;
+        enderecos[i] = endereco;
+        distancias[i] = distancia;
+        situacoes[i] = situacao;
+
+        
+    
+    
+
+}while( opcao != n)
+
