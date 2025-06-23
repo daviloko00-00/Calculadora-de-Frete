65b0237d5d13595665cb66fb1c95bbd18eba6a40
Davi S. <149006867+daviloko00-00@users.noreply.github.com>
2025-06-23 13:07:17 -0300

    Código finalizado e comentado

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const promptSync = require("prompt-sync")();
-let nomes = [];
+
+let nomes = [];// declarção das variáveis que serão utilizadas e arrays para armazenamento de dados;
+
 let enderecos = [];
 let distancias = [];
 let situacoes = [];
@@ -16,10 +18,13 @@ let valor;
 
 const fs = require("fs");
 const path = require("path");
-fs.writeFileSync("listagem completa.txt", "", "utf8");
+
+fs.writeFileSync("listagem completa.txt", "", "utf8");// inicia um arquivo vazio para armazenar os dados dos clientes
+let valortotal;
 do {
     do{
-    
+        // Entrada de dados do cliente, com validação para garantir que os dados sejam inseridos corretamente.
+
         let nome = promptSync(`Digite o nome do clinte ${i}: `);
         let endereco = promptSync(`Digite o endereço do cliente ${i}: `);
         let distancia = promptSync(`Digite a distancia ${i}: `);
@@ -30,6 +35,8 @@ do {
         }
     } while ((!isNaN(nome)) || !isNaN(endereco) || isNaN(distancia) || isNaN(situacao) || isNaN(valor));
 
+    //calculo do valor total da viagem com base na situação do cliente
+
     if (situacao == 1) {
         situacao = "normal";
         valortotal = valor * distancia;
@@ -38,13 +45,18 @@ do {
         valortotal = valor + 0.2*valor;
     }
 
+    // Armazenamento dos dados inseridos pelo usuário nos arrays correspondentes
+
     nomes[i] = nome;
     enderecos[i] = endereco;
     distancias[i] = distancia;
     situacoes[i] = situacao;
     valores[i] = valor;
     valorestotais[i] = valortotal;
+    // Exibição dos dados do cliente e confirmação de registro
     cliente = console.log(`nome: ${nomes[i]}\n endereço: = ${enderecos[i]}\n distancia ${distancias[i]} \n situação : ${situacoes[i]}\n valor total : ${valorestotais[i].toFixed(2)} \n valor por km ${valores[i].toFixed(2)} `)
+    // Adiciona o cliente ao array de clientes
+
     opcao = promptSync(`Cliente registrado, caso deseja continuar tecle enter, caso não digite "n": `)
     cliente = clientes[i];
     if (opcao == "n") {
@@ -52,10 +64,15 @@ do {
 
     }
 
+    // Armazenamento dos dados no arquivo "listagem completa.txt", onde cada cliente é adicionado em uma nova linha com formatação adequada.
+
     let textoporlinha = `nome: ${nomes[i]}\n endereço: = ${enderecos[i]}\n distancia ${distancias[i]} \n situação : ${situacoes[i]}\n valor total : ${valorestotais[i].toFixed(2)} \n valor por km ${valores[i].toFixed(2)}`;
     fs.appendFileSync("listagem completa.txt", textoporlinha, "utf8");
     fs.appendFileSync("listagem completa.txt", "\n\n", "utf8")
 
     i++;
 } while (opcao !== "n");
+
+// Exibe uma mensagem indicando que o arquivo foi criado com sucesso.
+
 console.log("arquivo feito com a listagem completa")
